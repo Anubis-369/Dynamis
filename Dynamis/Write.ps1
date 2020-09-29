f0a86a8d1ee45335119a502a5f667bbdb0a7713f
Rin Nakamura <overmyself@nrin.net>
2020-09-29 22:25:05 +0900

    Add Write Function

--- Dynamis/Write.ps1
+++ Dynamis/Write.ps1
@@ -1,14 +1,111 @@
 
-Function Write-DyPSObject {
-    param(
-        [psobject]$Schema="",
-        [psobject]$PSO,
-        [string]$Encoding = "UTF8",
-        [string[]]$Properties = @()
+Function Convert-DefaultSchema {
+    param (
+        [Parameter(Mandatory=$true)][string]$PSO
     )
-    if($Properties.count -eq 0){$Properties = $PSO.psobject.properties }
-    if ( $Schema -eq "" ) { $Titlees = $Properties } else {
-        #$Properties | %{ $Schema }
+    $Properties = $PSO.psobject.properties.Name
+    $MaxCount = $Properties | % {
+        $_.length + ([System.Text.Encoding]::GetEncoding("utf-8").GetByteCount( $_ ) - $_.length) / 2
+    } | Measure-Object -Maximum | % Maximum
+
+    $KNo = 1
+
+    foreach ( $el in $Properties ) { 
+        $Len = $MaxCount - ( $el.length + `
+        ([System.Text.Encoding]::GetEncoding("utf-8").GetByteCount( $el ) - $el.length) / 2 )
+
+        New-Object PSObject -Property @{
+            DNo         = 0;
+            Data        = "";
+            KNo         = $KNo;
+            Key         = $el;
+            Value       = $el;
+            Type        = "String";
+            Option      = "";
+            Description = "";
+            Indent      = 2;
+            LongData    = $False;
+            Join        = $False;
+            TitleIndent = $Len;
+            DataIndent  = 0;
+            EndLine     = 0;
+            Fileinfo    = ""
+        } | Select-Object `
+        DNo,Data,KNo,Key,Type,Value,Option,Description,Indent,Join,LongData,
+        TitleIndent,DataIndent,EndLine,Fileinfo
+
+        $KNo += 1
+    }
+}
+
+Function Convert-BaseData {
+    param(
+        [psobject]$PSO,
+        [psobject]$Schema,
+        [string]$DataName = ""
+    )
+    $Result = ""
+    $Schema = $Schema | ? { $_.Data -eq $DataName }
+    foreach ( $el in $Schema ) {
+        $Title        = $el.Value
+        $Value        = $PSO | % ($el.Key)
+        $Title_Indent = " "  * $el.TitleIndent
+        $Endline      = "`n" * $el.EndLine
+
+        if(($el.LongData -eq $True) -and ($el.Join -eq $False)) {
+            $Indent = "`n" + " " * $el.Indent
+            $Value  = " " * $el.Indent + ($Value -replace("`n",$Indent)).Trim()
+            $Result += "`n{0}:`n{1}{2}" -f $Title,$Value,$Endline
+            $CanJoin = $False
+
+        } elseif (($el.LongData -eq $False ) -and ($el.Join -eq $True) -and ( $CanJoin -eq $True)) {
+            $DataIndent = " " * $DataIndentCount
+            $Result += "{3} , {0}{2}: {1}{4}" -f $Title,$Value.Trim(),$Title_Indent,$DataIndent,$Endline
+            $CanJoin = $True
+
+            if ( $Endline.length -eq 0) {
+                $CanJoin = $True
+                $DataIndentCount = $el.DataIndent - ( $Value.length + `
+                ([System.Text.Encoding]::GetEncoding("utf-8").GetByteCount( $Value ) - $Value.length) / 2 )
+            } else {
+                $CanJoin = $False
+            }
+
+        } else {
+            $Value = $Value.Trim()
+            $Result += "`n{0}{2}: {1}{3}" -f $Title,$Value,$Title_Indent,$Endline
+
+            if ( $Endline.length -eq 0) {
+                $CanJoin = $True
+                $DataIndentCount = $el.DataIndent - ( $Value.length + `
+                ([System.Text.Encoding]::GetEncoding("utf-8").GetByteCount( $Value ) - $Value.length) / 2 )
+            } else {
+                $CanJoin = $False
+            }
+        }
+    }
+}
+
+
+Function Write-DyPSO {
+    param(
+        [psobject[]]$PSO,
+        [string]$Schema = "",
+        [string]$DataName = "",
+        [string]$Encoding = "UTF8"
+    )
+    begin {
+        if ( Test-Path $Schema) {$Scm = Read-DySchema $Schema -Encoding $Encoding }
+        $Result = @()
+    }
+    Process {
+        foreach ( $el in $PSO) {
+            if (!(Test-Path $Schema)) { $Scm = Convert-DefaultSchema $el }
+            $Result += Convert-BaseData -PSO $PSO -Schema $Scm -DataName $DataName
+        }
+    }
+    end {
+        $Result.Join("`n`n`n----`n")
     }
 }
 
